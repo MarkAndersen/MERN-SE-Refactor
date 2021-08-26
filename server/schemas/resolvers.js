3190eac3d0bc0bec807ccd200463ff9fce9307e1
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-08-26 08:43:22 -0700

    finished basic resolvers, will test and cleanup

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,66 @@
+const { signToken } = require('../utils/auth');
+const { User } = require('../models')
+
+const resolvers = {
+    Query: {
+        user: async (parent, args, context) => {
+            return User.findOne({ _id: context.user._id }).populate('books');
+        }
+//params...
+    },
+
+    Mutation: {
+
+        addUser: async (parent, args) => {
+            return User.create(args)
+        },
+
+        login : async (parent, { email, password}) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('No user with this registered with this email!');
+            }
+            
+            const verifyPw = await user.isCorrectPassword(password);
+            
+            if (!verifyPw) { 
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+
+            return { token, user };
+         },
+
+         saveBook: async (parents, { savedBooks }, context) => {
+            console.log(args)
+            if (context.user){
+            return await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks } },
+                { new: true, runValidators: true }
+              )
+            };
+              throw new AuthenticationError('Not logged in');
+    },
+
+        // deleteBook: async (parents, { savedBooks }, context) => {
+        //     console.log(args)
+        //     if(context.user) {
+        //         const removeBook =  await User.findOneAndDelete(
+        //             { _id: context.user._id },
+        //             {$pull: { savedBooks: bookIds }}
+
+        //         )
+        //     }
+
+        // }
+
+
+
+         }
+
+    },
+};
+
+module.exports = resolvers;
